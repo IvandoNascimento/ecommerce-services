6cf402ae9dae55d8fff3a00b755fd07910b4a161
IvandoNascimento <nascimentodoivan@gmail.com>
2024-09-27 14:17:59 -0300

    feat: movendo para arquvio de config e criando realm separado

--- auth/src/main/java/br/com/ufape/es/topicos/auth/config/SecurityConfig.java
+++ auth/src/main/java/br/com/ufape/es/topicos/auth/config/SecurityConfig.java
@@ -1,8 +1,10 @@
-package br.com.ufape.es.topicos.auth;
+package br.com.ufape.es.topicos.auth.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+//import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
 import org.springframework.security.oauth2.server.resource.authentication.JwtGrantedAuthoritiesConverter;
@@ -10,27 +12,36 @@ import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 import org.springframework.stereotype.Component;
 
+import br.com.ufape.es.topicos.auth.KeycloakAuthentication;
+
 @Configuration
 @Component
+//@EnableWebFluxSecurity
 public class SecurityConfig {
 
+    @Autowired
+    private KeycloakAuthentication keycloakAuthentication;
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        System.out.println("ttttaa");
         http
             .csrf(csrf -> csrf.disable()) // Desabilitar CSRF para simplificação
             .authorizeHttpRequests(authorize -> authorize
-                .requestMatchers(new AntPathRequestMatcher("/api/gerente/**")).hasRole("gerente")
-                .requestMatchers(new AntPathRequestMatcher("/api/usuario/**")).hasAnyRole("usuario", "gerente")
+                .requestMatchers(new AntPathRequestMatcher("/api/gerente/**")).hasRole("ECOMMERCE-CLIENTGERENTE")
+                .requestMatchers(new AntPathRequestMatcher("/api/usuario/**")).hasAnyRole("ECOMMERCE-CLIENTUSUARIO", "ECOMMERCE-CLIENTGERENTE","OFFLINE_ACCESS")
+                
                 .anyRequest().permitAll()
             )
-            .oauth2ResourceServer(oauth2 -> oauth2.jwt(jwt -> jwt.jwtAuthenticationConverter(jwtAuthenticationConverter())));
+            //.authorizeExchange(exchange -> exchange
+            //    .pathMatchers("/eureka/**").permitAll()
+            //)
+            
+            .oauth2ResourceServer(oauth2 -> oauth2.jwt(jwt -> jwt.jwtAuthenticationConverter(keycloakAuthentication)));
 
         return http.build();
     }
 
-    private JwtAuthenticationConverter jwtAuthenticationConverter() {
+    /*private JwtAuthenticationConverter jwtAuthenticationConverter() {
         JwtGrantedAuthoritiesConverter grantedAuthoritiesConverter = new JwtGrantedAuthoritiesConverter();
         System.out.println("tttt");
         grantedAuthoritiesConverter.setAuthoritiesClaimName("realm_access.roles");
@@ -40,5 +51,5 @@ public class SecurityConfig {
         jwtAuthenticationConverter.setJwtGrantedAuthoritiesConverter(grantedAuthoritiesConverter);
 
         return jwtAuthenticationConverter;
-    }
+    }*/
 }
